6d86a4c7d05fd64408e2939350375e57e5e6dd64
BrianMath <brianmatheus10@gmail.com>
2022-09-13 10:52:35 -0300

    Melhoria de miniprojeto Tentei, mas até agora não funfa

--- Lista5_Estruturas/MiniProjetoUnificado.c
+++ Lista5_Estruturas/MiniProjetoUnificado.c
@@ -12,53 +12,88 @@ typedef struct {
 } Poly;
 
 
-void lePolinomio(Poly *poly) {
-	char coef[sizeString];
-	int pot, i = 0, maiorPot;
+void lePolinomio(double *coeficientes) {
+	int pot, maiorPot = -1;
+	char digito0[2], coefParcial[20], coefFinal[20];
 
-	// 20x^3+1x^2-5x^1
 	while (1) {
-		scanf("%[^x||\n]%*c%*c%d", coef, &pot);
+		scanf("%c", digito0);
 
-		if (i == 0) {
-			maiorPot = pot;
-			poly->coef = (Poly *) realloc(poly->coef, (maiorPot) * sizeof(Poly));
-		} else if (pot > maiorPot) {
-			maiorPot = pot;
-			poly->coef = (Poly *) realloc(poly->coef, (maiorPot) * sizeof(Poly));
+		if (digito0[0] == '\n') {
+			break;
 		}
+		
+		scanf("%[^x]%*c%*c", coefParcial);
 
-		poly->coef[pot] = atof(coef);
+		memset(coefFinal, 0, sizeof(coefFinal));
+		strcat(coefFinal, digito0);
+		strcat(coefFinal, coefParcial);
+
+		scanf("%d", &pot);
+		if (pot > maiorPot) {
+			maiorPot = pot;
+			coeficientes = (double *) realloc(coeficientes, maiorPot * sizeof(double));
+		}
+		
+		coeficientes[pot] = atof(coefFinal);
 	}
 }
 
 
 int main() {
-	char nomePolinomio[sizeString];
-	int sizeId, i = 0, coeficiente, potencia, maiorPot;
+	char idPolinomio[sizeString];
+	int sizeId, i;
+	int qtdPolinomios;
 
 	Poly *polinomio = NULL;
 	// Inicialmente 'polinomio' receberá a memória para somente
 	// 1 polinômio e receberá mais se necessário
 	polinomio = (Poly *) malloc(sizeof(Poly));
 
-	while (scanf("%[^:]%*c%*c", nomePolinomio) != EOF) {
-		// Realoca memória para caber mais um polinômio
-		polinomio = (Poly *) realloc(polinomio, (i+1) * sizeof(Poly));
+	scanf("%d", &qtdPolinomios);
 
-		// Pega o tamanho da string de id, ...
-		sizeId = strlen(nomePolinomio);
-		// ... aloca a memória no atributo id da struct ...
-		polinomio[i].id = (char *) calloc(sizeId, sizeof(char));
-		// ... e copia a string lida para o atributo
-		strcpy(polinomio[i].id, nomePolinomio);
+	/* A: 1x^2+3x^9-2x^0 */
 
-		// Aloca espaço para 1 double inicialmente e mais se necessário
+	// Recebe e aloca tudo dos polinômios
+	for (i = 0; i < qtdPolinomios; i++) {
+		// Lê o id e calcula o tamanho da string
+		scanf("%*c%[^:]%*c%*c", idPolinomio);
+		sizeId = strlen(idPolinomio);
+
+		// Atributo .id é um ponteiro para a memória onde estará a string de nome
+		polinomio[i].id = (char *) calloc(sizeId+1, sizeof(char));
+		// Copia a string lida para o atributo .id
+		strcpy(polinomio[i].id, idPolinomio);
+
+		// Atributo .coef é um ponteiro para a memória onde estará o vetor de coeficientes
 		polinomio[i].coef = (double *) calloc(1, sizeof(double));
+
 		// Lê o corpo do polinômio
-		lePolinomio(&polinomio[i]);
+		lePolinomio(polinomio[i].coef);
+
+		printf("\n\n----------\n");
+
+		for (int i = 0; i < 10; i++) {
+			if (polinomio[0].coef[i] > 0 || polinomio[0].coef[i] < 0) {
+				if (i > 0 && polinomio[0].coef[i] >= 0) {
+					printf("+");
+				}
+				
+				printf("%.2fx^%d", polinomio[0].coef[i], i);
+			}
+		}
+		
+		// // Realoca memória para caber mais um polinômio
+		// polinomio = (Poly *) realloc(polinomio, (i+1) * sizeof(Poly));
 	}
 	
+	for (i = 0; i < qtdPolinomios; i++) {
+		free(polinomio[i].id);
+	}
+	for (i = 0; i < qtdPolinomios; i++) {
+		free(polinomio[i].coef);
+	}
+	free(polinomio);
 
 	return 0;
 }

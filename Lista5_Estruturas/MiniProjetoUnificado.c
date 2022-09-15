63d1ec5074d02e3a8d1aaec6131c5c730655e439
BrianMath <brianmatheus10@gmail.com>
2022-09-14 22:30:38 -0300

    Miniprojeto finalizado Finalmente consegui fazer o miniprojeto unificado

--- Lista5_Estruturas/MiniProjetoUnificado.c
+++ Lista5_Estruturas/MiniProjetoUnificado.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define sizeString 100
+#define sizeString 200
 
 
 typedef struct {
@@ -12,53 +12,165 @@ typedef struct {
 } Poly;
 
 
-void lePolinomio(double *coeficientes) {
-	int pot, maiorPot = -1;
-	char digito0[2], coefParcial[20], coefFinal[20];
+void lePolinomio(Poly *polinomio) {
+	char string[sizeString] = {0}, temp[sizeString] = {0};
+	int i, j = 0, expoente = -1, antesExp = 0;
 
-	while (1) {
-		scanf("%c", digito0);
+	scanf("%[^\n]%*c", string);
 
-		if (digito0[0] == '\n') {
-			break;
+	// Achar maior expoente
+	for (i = 0; i < strlen(string); i++) {
+		if (string[i] == '^' && atoi(&string[i+1]) > expoente) {
+			expoente = atoi(&string[i+1]);
 		}
-		
-		scanf("%[^x]%*c%*c", coefParcial);
-
-		memset(coefFinal, 0, sizeof(coefFinal));
-		strcat(coefFinal, digito0);
-		strcat(coefFinal, coefParcial);
-
-		scanf("%d", &pot);
-		if (pot > maiorPot) {
-			maiorPot = pot;
-			coeficientes = (double *) realloc(coeficientes, (maiorPot * sizeof(double)) + 1);
-		}
-		
-		coeficientes[pot] = atof(coefFinal);
 	}
+	
+	// Atribuir o maior expoente para o polinômio atual
+	(*polinomio).grauPoly = expoente;
+	// E alocar memória para o vetor de coeficientes
+	(*polinomio).coef = (double *) calloc(expoente+1, sizeof(double));
+
+	/* A: 2x^2+3x^9-2x^0 */
+	/* Boi: 13x^5+3x^2-9x^1 */
+	/* Tigre: -1x^1-2x^4+11x^2 */
+	// Conferir um caractere por vez
+	for (i = 0; i < strlen(string); i++) {
+		// Se for 'x' ou '^': antecede o expoente, então ativa a flag e pula
+		if (string[i] == 'x' || string[i] == '^') {
+			antesExp = 1;
+			continue;
+		}
+
+		// Se a flag está ativada: está no expoente
+		if (antesExp) {
+			expoente = atoi(&string[i]);
+
+			// Põe tudo o que pegou até agora (coeficientes) pra dentro
+			(*polinomio).coef[expoente] = atof(temp);
+			
+			// Zera tudo e continua para o próximo caractere (um coeficiente)
+			memset(temp, 0, sizeof(temp));
+			j = 0;
+			antesExp = 0;
+
+			continue;
+		}
+		
+		// temp recebe os caracteres em ordem até acabarem os coeficientes
+		temp[j] = string[i];
+		j++;
+	}
+}
+
+void realizaOperacao(Poly *polinomio, int qtdPoli) {
+	char id0[sizeString/2] = {0}, id1[sizeString/2] = {0};
+	char operacao[2];
+	int i, posPoli0, posPoli1, grauPoli0, grauPoli1, maiorExp, menorExp;
+	double termo;
+
+	// Ler os dois ids e a operação entre eles
+	scanf("%[^+||-]", id0);
+	scanf("%c", operacao);
+	scanf("%[^\n]", id1);
+
+	// Achar as posições dos dois ids no vetor de polinômios 
+	for (i = 0; i < qtdPoli; i++) {
+		if (strcmp(polinomio[i].id, id0) == 0) {
+			posPoli0 = i;
+			grauPoli0 = polinomio[i].grauPoly;
+		}
+		// if (strlen(polinomio[i].id) == strlen(id0)) {
+		// }
+
+		if (strcmp(polinomio[i].id, id1) == 0) {
+			posPoli1 = i;
+			grauPoli1 = polinomio[i].grauPoly;
+		}
+		// if (strlen(polinomio[i].id) == strlen(id1)) {
+		// }
+	}
+	
+	// Achar maior expoente entre os dois polinomios
+	if (grauPoli0 > grauPoli1) {
+		maiorExp = grauPoli0;
+		menorExp = grauPoli1;
+	} else {
+		maiorExp = grauPoli1;
+		menorExp = grauPoli0;
+	}
+	
+	// Fazer as contas
+	for (i = 0; i <= maiorExp; i++) {
+		if (maiorExp == grauPoli0 && menorExp < maiorExp) {
+			if (i > menorExp) {
+				if (operacao[0] == '+') {
+					termo = polinomio[posPoli0].coef[i];
+				} else {
+					termo = polinomio[posPoli0].coef[i];
+				}
+			} else {
+				if (operacao[0] == '+') {
+					termo = polinomio[posPoli0].coef[i] + polinomio[posPoli1].coef[i];
+				} else {
+					termo = polinomio[posPoli0].coef[i] - polinomio[posPoli1].coef[i];
+				}
+			}
+		} else if (maiorExp == grauPoli1 && menorExp < maiorExp) {
+			if (i > menorExp) {
+				if (operacao[0] == '+') {
+					termo = polinomio[posPoli1].coef[i];
+				} else {
+					termo = -polinomio[posPoli1].coef[i];
+				}
+			} else {
+				if (operacao[0] == '+') {
+					termo = polinomio[posPoli1].coef[i] + polinomio[posPoli0].coef[i];
+				} else {
+					termo = polinomio[posPoli0].coef[i] - polinomio[posPoli1].coef[i];
+				}
+			}
+		} else if (maiorExp == menorExp) {
+			if (operacao[0] == '+') {
+				termo = polinomio[posPoli0].coef[i] + polinomio[posPoli1].coef[i];
+			} else {
+				if (maiorExp == grauPoli0) {
+					termo = polinomio[posPoli0].coef[i] - polinomio[posPoli1].coef[i];
+				} else if (maiorExp == grauPoli1) {
+					termo = polinomio[posPoli1].coef[i] - polinomio[posPoli0].coef[i];
+				}
+			}			
+		}
+
+		if (termo != 0 && termo != -0) {
+			if (i > 0 && termo > 0) {
+				printf("+");
+			}
+			
+			printf("%.0fx^%d", termo, i);
+		}
+	}
+
+	printf("\n");
+	scanf("%*c");
 }
 
 
 int main() {
-	char idPolinomio[sizeString];
-	int sizeId, i;
-	int qtdPolinomios;
+	char idPolinomio[sizeString] = {0};
+	int sizeId, qtdPoli = 0, i, j;
+	int qtdPolinomios, qtdOperacoes;
 
 	Poly *polinomio = NULL;
 	// Inicialmente 'polinomio' receberá a memória para somente
 	// 1 polinômio e receberá mais se necessário
 	polinomio = (Poly *) malloc(sizeof(Poly));
 
-	scanf("%d", &qtdPolinomios);
-
-	/* 1 */
-	/* A: 20x^2+3x^9-2x^0 */
+	scanf("%d%*c", &qtdPolinomios);
 
 	// Recebe e aloca tudo dos polinômios
 	for (i = 0; i < qtdPolinomios; i++) {
 		// Lê o id e calcula o tamanho da string
-		scanf("%*c%[^:]%*c%*c", idPolinomio);
+		scanf("%[^:]%*c%*c", idPolinomio);
 		sizeId = strlen(idPolinomio);
 
 		// Atributo .id é um ponteiro para a memória onde estará a string de nome
@@ -66,28 +178,34 @@ int main() {
 		// Copia a string lida para o atributo .id
 		strcpy(polinomio[i].id, idPolinomio);
 
-		// Atributo .coef é um ponteiro para a memória onde estará o vetor de coeficientes
-		polinomio[i].coef = (double *) calloc(1, sizeof(double));
-
 		// Lê o corpo do polinômio
-		lePolinomio(polinomio[i].coef);
+		lePolinomio(&polinomio[i]);
 
-		// printf("\n\n----------\n");
+		// for (j = 0; j <= polinomio[i].grauPoly; j++) {
+		// 	if (polinomio[i].coef[j] > 0) {
+		// 		if (j > 0) {
+		// 			printf("+");
+		// 		}
 
-		for (int i = 0; i < 10; i++) {
-			if (polinomio[0].coef[i] != 0) {
-				if (i > 0 && polinomio[0].coef[i] > 0) {
-					printf("+");
-				}
-				
-				printf("%.0fx^%d", polinomio[0].coef[i], i);
-			}
-		}
+		// 		printf("%.0fx^%d", polinomio[i].coef[j], j);
+		// 	} else if (polinomio[i].coef[j] < 0) {
+		// 		printf("%.0fx^%d", polinomio[i].coef[j], j);
+		// 	}
+		// }
+		// printf("\n");
 		
-		// // Realoca memória para caber mais um polinômio
-		// polinomio = (Poly *) realloc(polinomio, (i+1) * sizeof(Poly));
+		// Realoca memória para caber mais um polinômio
+		polinomio = (Poly *) realloc(polinomio, (i+2) * sizeof(Poly));
+		qtdPoli++;
 	}
 	
+	scanf("%d%*c", &qtdOperacoes);
+
+	// Recebe e realiza as operações com os polinômios salvos
+	for (i = 0; i < qtdOperacoes; i++) {
+		realizaOperacao(polinomio, qtdPoli);
+	}
+
 	for (i = 0; i < qtdPolinomios; i++) {
 		free(polinomio[i].id);
 	}
